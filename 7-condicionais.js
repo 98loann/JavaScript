c3fe87fea87c170decae05d19c20a873602957bf
98loann <98luan@gmail.com>
2023-04-19 18:05:01 -0300

    ...

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -12,6 +12,7 @@ const listaDeDestinos = new Array(
 //listaDeDestinos.push(`Belo Horizonte`)//inserindo item ao Array
 
 const idadeComprador=15
+const estaAcompanhada=true
 console.log("Destinos possíveis: ")
 console.log(listaDeDestinos)
 
@@ -19,6 +20,11 @@ if (idadeComprador >= 18){
     console.log(`Comprador maior de idade`)
     listaDeDestinos.splice(1,1) //removendo item
 }else{
+    if(estaAcompanhada){
+        console.log(`Comprador está acompanhada`)
+    }else{
+        console.log(`Não está acompanhada`)
+    }
     console.log(`Comprador menor de idade, não é possível realizar a compra`)
 }
 console.log(listaDeDestinos)
